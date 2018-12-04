c9cfed36a6b6694228ce89a97b73826e28f3e407
Nyabongo E.T <nyabongoedgar@gmail.com>
2018-12-04 14:33:50 +0300

    adds admin.js file

---
+++ UI/js/admin.js
@@ -0,0 +1,40 @@
+function change_parcel_destination(){
+    const new_area = document.getElementById('new_area').value;
+    var data = {
+        destination : new_area
+    };
+    token = localStorage.getItem('token');
+    if (token){
+    api_object.update(update_url,data,token)
+    .then(resp_data =>{
+        window.alert(resp_data);
+    })
+    .catch(error => {
+        return document.getElementById('error').innerHTML = JSON.stringify=(error)
+    } ); 
+    } 
+    else{
+        window.alert('Token missing !');
+    }
+}
+
+
+function change_parcel_destination(){
+    const new_area = document.getElementById('new_area').value;
+    var data = {
+        destination : new_area
+    };
+    token = localStorage.getItem('token');
+    if (token){
+    api_object.update(update_url,data,token)
+    .then(resp_data =>{
+        window.alert(resp_data);
+    })
+    .catch(error => {
+        return document.getElementById('error').innerHTML = JSON.stringify=(error)
+    } ); 
+    } 
+    else{
+        window.alert('Token missing !');
+    }
+}
